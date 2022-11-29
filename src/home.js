dbf4119db67c4f0dd36ebb77034a608ea32571c4
Aminady <aminasady56@gmail.com>
2022-11-29 12:42:19 +0000

    some more modules

--- src/home.js
+++ src/home.js
@@ -4,8 +4,10 @@ presentation.className = 'presentation';
 
 presentation.innerHTML = `
     <p>
-        Tic-tac-toe or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. 
+        Tic-tac-toe or Xs and Os is a paper-and-pencil game for two players who take turns marking the spaces in a three-by-three grid with X or O. <br> The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. <br>
 
         Games played on three-in-a-row boards can be traced back to ancient Egypt, where such game boards have been found on roofing tiles dating from around 1300 BC.
 
-    </p>`
+    </p>`;
+
+export {presentation};
